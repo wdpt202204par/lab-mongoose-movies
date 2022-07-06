a7ee890a7ff8027170e21b0abaf9a8ae6a32d301
WilsonF78 <fortes.wilson78@gmail.com>
2022-07-06 19:45:21 +0200

    Iteration 3 en cour

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,22 +1,30 @@
-const express       = require('express');
-const router        = express.Router();
-const Mongoose      = require('mongoose');
-const Celebrities   = require('../models/Celebrity.model')
-let celebritiesData
+const express = require("express");
+const router = express.Router();
+const Mongoose = require("mongoose");
+const Celebrities = require("../models/Celebrity.model");
+let celebritiesData;
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-
-    Celebrities.find()
-    .then(CelebritiesFromDB =>{
-        celebritiesData = CelebritiesFromDB
+router.get("/", (req, res, next) => {
+  Celebrities.find()
+    .then((CelebritiesFromDB) => {
+      celebritiesData = CelebritiesFromDB;
+      res.render("celebrities/index", {
+        data: celebritiesData,
+      });
     })
-    .catch(err => next(err))
-    res.render('celebrities/index',{
-        data : celebritiesData
-  });
+    .catch((err) => next(err));
 });
 
+router.get("/:id", (req, res, next) => {
+  Celebrities.findById()
+    .then((CelebritiesFromDB) => {
+      res.render("celebrities/show", {
+        data: CelebritiesFromDB,
+      });
+    })
 
+    .catch((err) => next(err));
+});
 
 module.exports = router;
