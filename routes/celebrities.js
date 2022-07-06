175699da5b6ccc91b5d95866dcac52928cfe88c4
Alan ROGER <aa.roger404@gmail.com>
2022-07-06 19:44:30 +0200

    Iteration 4 done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -7,7 +7,7 @@ router.get('/', (req, res, next) => {
     Celebrity.find()
         .then(function (celebritiesFromDB) {
             console.log('celebritiesFromDB:', celebritiesFromDB);
-            res.render('celebrities/index', { celebrities : celebritiesFromDB });
+            res.render('celebrities/index', { celebrities: celebritiesFromDB });
         })
         .catch(err => {
             console.log(err);
@@ -16,16 +16,70 @@ router.get('/', (req, res, next) => {
 
 })
 
-router.get('/:id',(req,res,next)=>{
-    Celebrity.findById(req.params.id)
-    .then(function (celebrityFromDB) {
-        console.log("celebrityFromDB=", celebrityFromDB);
-        res.render("celebrities/show", {celebrity: celebrityFromDB,});
-      })
-      .catch((err) => {
-        console.log(err);
-        next(err);
-      });
+router.get('/new', (req, res, next) => {
+    res.render('celebrities/new', {})
 })
+
+router.post('/new', (req, res, next) => {
+    const name = req.body.name
+    const occupation = req.body.occupation
+    const catchphrase = req.body.catchphrase
+    console.log(name, occupation, catchphrase)
+
+    const newCeleb =  new Celebrity({
+        name: name,
+        occupation: occupation,
+        catchphrase: catchphrase
+    })
+
+    newCeleb.save()
+    .then((newCelebrity) => {
+        console.log('nouvelle célébrité ajoutée à la DB', newCelebrity)
+        res.redirect("/celebrities")
+    })
+    .catch(err => {
+        console.log("problème à la création d'une célébrité", err)
+        res.render('celebrities/new',{})
+        next(err)
+    })
+
+    // Celebrity.create({
+    //     name: name,
+    //     occupation: occupation,
+    //     catchphrase: catchphrase
+    // })
+    //     .then((newCeleb) => {
+    //         console.log('nouvelle célébrité ajoutée à la DB', newCeleb)
+    //         res.redirect("/celebrities")
+    //     })
+    //     .catch(err => {
+    //         console.log("problème à la création d'une célébrité", err)
+    //         res.render('celebrities/new',{})
+    //         next(err)
+    //     })
+
+})
+
+
+
+
+
+
+
+
+router.get('/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+        .then(function (celebrityFromDB) {
+            console.log("celebrityFromDB=", celebrityFromDB);
+            res.render("celebrities/show", { celebrity: celebrityFromDB, });
+        })
+        .catch((err) => {
+            console.log(err);
+            next(err);
+        });
+})
+
+
+
 
 module.exports = router;
