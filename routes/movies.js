369c315f0c45bab37ccbe4d7af82455315f555bd
WilsonF78 <fortes.wilson78@gmail.com>
2022-07-09 12:24:55 +0200

    Iteration 9 OK

--- routes/movies.js
+++ routes/movies.js
@@ -1,27 +1,35 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const Celebrities = require("../models/Celebrity.model");
 const Movies = require("../models/Movie.model");
 
-router.get("/new", (req, res, next) =>{
-    Celebrities.find()
-    .then(CelebritiesFromDB =>{
-        res.render("movies/new", {
-            CelebritiesData: CelebritiesFromDB
-        })
+router.get("/new", (req, res, next) => {
+  Celebrities.find()
+    .then((CelebritiesFromDB) => {
+      res.render("movies/new", {
+        CelebritiesData: CelebritiesFromDB,
+      });
     })
-    .catch(err => next(err))
-})
+    .catch((err) => next(err));
+});
 
-router.post("/", (req, res, next) =>{
-    Movies.create({
-        title : req.body.title,
-        genre : req.body.genre,
-        plot  : req.body.plot,
-        cast  : req.body.cast
+router.post("/", (req, res, next) => {
+  Movies.create({
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot,
+    cast: req.body.cast,
+  });
+});
+
+router.get("/", (req, res, next) => {
+  Movies.find()
+    .then((MoviesFromDB) => {
+      res.render("movies/index", {
+        MoviesData: MoviesFromDB,
+      });
     })
-})
-
-
+    .catch((err) => next(err));
+});
 
 module.exports = router;
