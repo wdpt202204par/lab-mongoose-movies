2a596876242ae1fdf404d017f3f5f117eaca0409
WilsonF78 <fortes.wilson78@gmail.com>
2022-07-09 16:42:31 +0200

    Iteration 11 OK

--- routes/movies.js
+++ routes/movies.js
@@ -9,7 +9,11 @@ router.post("/", (req, res, next) => {
     genre: req.body.genre,
     plot: req.body.plot,
     cast: req.body.cast,
-  });
+  })
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => next(err));
 });
 
 router.get("/", (req, res, next) => {
@@ -23,25 +27,33 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/new", (req, res, next) => {
-    Celebrities.find()
-      .then((CelebritiesFromDB) => {
-        res.render("movies/new", {
-          CelebritiesData: CelebritiesFromDB,
-        });
-      })
-      .catch((err) => next(err));
-  });
+  Celebrities.find()
+    .then((CelebritiesFromDB) => {
+      res.render("movies/new", {
+        CelebritiesData: CelebritiesFromDB,
+      });
+    })
+    .catch((err) => next(err));
+});
 
 router.get("/:id", (req, res, next) => {
-    Movies.findById(req.params.id)
+  Movies.findById(req.params.id)
     .populate("cast")
-    .then(MovieFromDB =>{
-        console.log(MovieFromDB)
-        res.render("movies/show", {
-            MovieData : MovieFromDB
-        })
+    .then((MovieFromDB) => {
+      console.log(MovieFromDB);
+      res.render("movies/show", {
+        MovieData: MovieFromDB,
+      });
     })
-    .catch(err => next(err))
-})
+    .catch((err) => next(err));
+});
+
+router.post("/:id/delete", (req, res, next) => {
+  Movies.findByIdAndRemove(req.params.id)
+    .then(function () {
+      res.redirect("/movies");
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = router;
