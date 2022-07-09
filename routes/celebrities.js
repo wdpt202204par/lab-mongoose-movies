09de377476ceabce340c8a0e2763733bc66b5adc
WilsonF78 <fortes.wilson78@gmail.com>
2022-07-09 10:31:19 +0200

    It 5

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -16,20 +16,20 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-    Celebrities.create({
-        name : req.body.name,
-        occupation : req.body.occupation,
-        catchPhrase : req.body.catchPhrase,
+  Celebrities.create({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  })
+    .then(function () {
+      res.redirect("/celebrities");
     })
-    .then(function() {
-        res.redirect('/celebrities')
-    })
-    .catch(err => next(err))
-})
+    .catch((err) => next(err));
+});
 
 router.get("/new", (req, res, next) => {
-    res.render("celebrities/new")
-})
+  res.render("celebrities/new");
+});
 
 router.get("/:id", (req, res, next) => {
   Celebrities.findById(req.params.id)
@@ -37,6 +37,14 @@ router.get("/:id", (req, res, next) => {
       res.render("celebrities/show", {
         data: CelebritiesFromDB,
       });
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/celebrities/:id/delete", (req, res, next) => {
+  Celebrities.findByIdAndRemove(req.params.id)
+    .then(function () {
+      res.redirect("/celebrities");
     })
     .catch((err) => next(err));
 });
